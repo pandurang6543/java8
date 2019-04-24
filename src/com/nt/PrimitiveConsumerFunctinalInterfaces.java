3fb9b7927bfcedcea5dec642d91075e2f6b3dbfb
Pandurang Mane <pandurang6543@gmail.com>
2019-04-24 18:33:11 +0530

    primitive consumer added

--- src/com/nt/PrimitiveConsumerFunctinalInterfaces.java
+++ src/com/nt/PrimitiveConsumerFunctinalInterfaces.java
@@ -1,5 +1,31 @@
 package com.nt;
 
-public class PrimitiveConsumerFunctinalInterfaces {
+import java.util.function.DoubleConsumer;
+import java.util.function.IntConsumer;
+import java.util.function.LongConsumer;
+import java.util.function.ObjDoubleConsumer;
+import java.util.function.ObjIntConsumer;
+import java.util.function.ObjLongConsumer;
 
+public class PrimitiveConsumerFunctinalInterfaces {
+public static void main(String[] args) {
+	IntConsumer c1=(i1)->System.out.println(i1);
+	c1.accept(10);
+	
+	LongConsumer c2=(i1)->System.out.println(i1);
+	c2.accept(10);
+	
+	DoubleConsumer c3=(i1)->System.out.println(i1);
+	c3.accept(10);
+	
+	ObjIntConsumer<String> c4=(s,i)->System.out.println("Name :"+s+" age :"+i);
+	c4.accept("Pandurang", 25);
+	
+	ObjLongConsumer<String> c5=(s,i)->System.out.println("Name :"+s+" age :"+i);
+	c5.accept("Pandurang", 25);
+	
+	ObjDoubleConsumer<String> c6=(s,i)->System.out.println("Name :"+s+" age :"+i);
+	c6.accept("Pandurang", 25);
+	
+}
 }
